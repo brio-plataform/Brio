0ce3e2c3765d4c115b4ae27dedbe965ad4158674
Maicon Pereira Barbosa <maicon.desenvolvedor@gmail.com>
2025-02-06 15:49:33 -0300

    :feat: Update Post Likes

--- src/components/FeedItem/feedItem.tsx
+++ src/components/FeedItem/feedItem.tsx
@@ -173,15 +173,7 @@ export function FeedItem({
     <>
       <Card className="w-full max-w-7xl mb-4 bg-muted/30 ">
         <CardHeader className="flex flex-row items-center gap-4">
-          <div className="flex flex-col items-center gap-1">
-            <Button variant="ghost" size="sm" onClick={handleLike}>
-              <ArrowBigUp className={cn("w-5 h-5 transition-colors", isLiked && "text-blue-500 [&>svg]:fill-blue-500")} />
-            </Button>
-            <span className="font-bold">{likeCount}</span>
-            <Button variant="ghost" size="sm" onClick={handleDesLike}>
-              <ArrowBigDown className={cn("w-5 h-5 transition-colors", isDesLiked && "text-red-500 [&>svg]:fill-red-500")} />
-            </Button>
-          </div>
+
           <Avatar>
             <AvatarImage src={author.avatar} alt={author.name} />
             <AvatarFallback className="bg-gray-500 text-muted-foreground">
@@ -373,6 +365,15 @@ export function FeedItem({
         <CardFooter className="flex flex-col gap-4">
           <div className="flex justify-between items-center w-full">
             <div className="flex gap-4">
+              <div className="flex items-center gap-1">
+                <Button variant="ghost" size="sm" onClick={handleLike}>
+                  <ArrowBigUp className={cn("w-5 h-5 transition-colors", isLiked && "text-blue-500 [&>svg]:fill-blue-500")} />
+                </Button>
+                <span className="font-bold">{likeCount}</span>
+                <Button variant="ghost" size="sm" onClick={handleDesLike}>
+                  <ArrowBigDown className={cn("w-5 h-5 transition-colors", isDesLiked && "text-red-500 [&>svg]:fill-red-500")} />
+                </Button>
+              </div>
               <Button variant="ghost" size="sm" onClick={() => setShowComments(!showComments)}>
                 <MessageSquare className="w-4 h-4 mr-2" />
                 {allComments.length}
